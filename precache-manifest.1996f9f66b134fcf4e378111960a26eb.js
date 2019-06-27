9029b64398538760410c011f81e347695f9c98f2
Felipe Nino <felipe@Felipes-MacBook-Pro.local>
2019-06-27 11:22:02 -0500

    Updates

---
+++ precache-manifest.1996f9f66b134fcf4e378111960a26eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3203618e974de5e904b9fcab7821732e",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "243d64e26fcb1f485b99",
+    "url": "/react-weather-app/static/css/main.24e3869c.chunk.css"
+  },
+  {
+    "revision": "e658fc5fe73a6a3a9790",
+    "url": "/react-weather-app/static/js/2.4cfc5fbd.chunk.js"
+  },
+  {
+    "revision": "243d64e26fcb1f485b99",
+    "url": "/react-weather-app/static/js/main.22b8a458.chunk.js"
+  },
+  {
+    "revision": "e736fc5812ad96aa92b3",
+    "url": "/react-weather-app/static/js/runtime~main.7e96bc80.js"
+  }
+]);
